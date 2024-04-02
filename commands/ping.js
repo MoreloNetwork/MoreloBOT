5b6e1b4e47c8cc5f2981aaae6e2b4b29b8e6df24
dmkng <themaking64@cock.li>
2024-04-02 03:03:42 +0200

    Small fix

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2024 dmkng
+ *
+ * This file is part of MoreloBOT.
+ *
+ * MoreloBOT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MoreloBOT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MoreloBOT.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 export default {
 	args_min: 0, args_max: 0, hide_msg: false,
 	admin_only: false, where: 0, cooldown: 5,
